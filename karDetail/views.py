d5c257d75265e188fa7a237921ecb6d4f877cfbb
Your Name <sanjida.ier@gmail.com>
2023-12-17 05:41:23 -0500

    second commit

--- karDetail/views.py
+++ karDetail/views.py
@@ -21,7 +21,8 @@ def add_car(request):
         form = CarForm()
 
     return render(request, 'detail.html', {'form': form})
-
+    
+@login_required
 class CarDetailView(DetailView):
     model = models.Car
     template_name = 'viewDetail.html'
